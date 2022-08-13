2e0ff60db05f361795555c15243663f9ae30383c
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-08-13 20:30:56 +0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
+const navBar = document.querySelector(".nav-bar");
+console.log(btnMobileNav, navBar);
+
+function handleOnClick(event) {
+  event.stopPropagation();
+  event.preventDefault();
+
+  const isOpen = navBar.classList.contains("nav--open");
+  navBar.classList.remove("nav--open");
+
+  if (isOpen) {
+    navBar.classList.remove("nav--open");
+  } else {
+    navBar.classList.add("nav--open");
+  }
+}
+
+btnMobileNav.addEventListener("click", handleOnClick);
+document.body.addEventListener("click", function (event) {
+  navBar.classList.remove("nav--open");
+});
